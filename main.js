e570d3845bdb0f3597302288dfb062cc7f2e4c79
jaiswalk008 <jaiskaran008@gmail.com>
2023-02-02 18:48:12 +0530

    task 11

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ form.addEventListener('submit' , onSubmit);
 function onSubmit(e){
     e.preventDefault();
     //setting item in local storage as (name,value) pair.
-    localStorage.setItem('User Name: ',''+e.target.name.value);
-    localStorage.setItem('User Email: ',''+e.target.email.value);
-    
+    localStorage.setItem('User Name: ',''+e.target.name.value,'User Email: ',''+e.target.email.value);
+    //localStorage.setItem('User Email: ',''+e.target.email.value);
+    // console.log(typeof localStorage.getItem('User Name: '));
+    let userData = {
+        userName : e.target.name.value,
+        userEmail : e.target.nemail.value
+    };
+    localStorage.setItem('User Data',JSON.stringify(userData));
+
+    //JSON.parse - to revert back to object form
+    e.target.name.value='';
+    e.target.email.value=''
 }
