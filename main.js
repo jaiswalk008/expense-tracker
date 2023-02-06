fa4bcfd70d0977213b2d5f94e5a7d4485dac2394
jaiswalk008 <jaiskaran008@gmail.com>
2023-02-06 19:10:59 +0530

    task 12

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 let form = document.getElementById('my-form');
 form.addEventListener('submit' , onSubmit);
-
+let users=[];
+let i=0;
 function onSubmit(e){
     e.preventDefault();
     //setting item in local storage as (name,value) pair.
+    //
     localStorage.setItem('User Name: ',''+e.target.name.value,'User Email: ',''+e.target.email.value);
     //localStorage.setItem('User Email: ',''+e.target.email.value);
     // console.log(typeof localStorage.getItem('User Name: '));
     let userData = {
         userName : e.target.name.value,
-        userEmail : e.target.nemail.value
+        userEmail : e.target.email.value,
+        userPhone : e.target.phone.value
     };
-    localStorage.setItem('User Data',JSON.stringify(userData));
+    users.push(userData);
 
+    localStorage.setItem(''+e.target.email.value,JSON.stringify(users[i]));
+    i++;
+    //console.log(users);
     //JSON.parse - to revert back to object form
     e.target.name.value='';
-    e.target.email.value=''
+    e.target.email.value='';
+    e.target.phone.value='';
 }
