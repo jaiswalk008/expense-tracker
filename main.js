043c8f5c3d4da075ed98b2bb0012509604762670
Karan Jaiswal <39873943+jaiswalk008@users.noreply.github.com>
2023-02-13 17:26:17 +0000

    added delete button

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let form = document.getElementById('my-form');
 form.addEventListener('submit' , onSubmit);
 let users=[];
 let i=0;
+const list =document.getElementById('user-list');
+let btn = document.createElement('button');
 function onSubmit(e){
     e.preventDefault();
     //setting item in local storage as (name,value) pair.
@@ -15,7 +17,15 @@ function onSubmit(e){
         userPhone : e.target.phone.value
     };
     users.push(userData);
-
+    let info = userData.userName +' - '+userData.userEmail+' - '+userData.userPhone;
+    let newLi = document.createElement('li');
+    
+    btn.textContent='delete';
+    btn.style.marginLeft='5px';
+    btn.className='delete';
+    newLi.appendChild(document.createTextNode(info));
+    newLi.appendChild(btn);
+    list.appendChild(newLi);
     localStorage.setItem(''+e.target.email.value,JSON.stringify(users[i]));
     i++;
     //console.log(users);
